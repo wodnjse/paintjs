8ba95a244281fbcef5505193085053b2de2cc014
wodnjse <macimaro04@gmail.com>
2022-08-08 23:57:36 +0900

    2-2 Recap!

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");  // canvas안에서의 픽셀을 다룰 수 있게 함
 
+canvas.width = 700;
+canvas.height = 700;
+
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
