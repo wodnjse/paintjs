5719a4e3077ee5391b9241ecb7a2a00d1dae1404
Jaewon Kim <macimaro04@gmail.com>
2022-08-20 02:23:21 +0900

    2-3 Changing Color

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");  // canvas안에서의 픽셀을 다룰 수 있게 함
+const colors =  document.getElementsByClassName("jsColor");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -29,13 +30,17 @@ function onMouseMove(event) {  // 캔버스 안에서의 마우스 좌표
     }
 }
 
-function onMouseDown(event) {  // 마우스를 클릭하면 칠하기 동작
-    painting = true;
+
+function handelColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
 
+/*
 function onMouseUp(event) {  // 마우스를 땠을 때, 칠하기를 멈춤
     stopPainting();
 }
+*/
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
@@ -43,3 +48,7 @@ if (canvas) {
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => 
+    color.addEventListener("click", handelColorClick)
+);
