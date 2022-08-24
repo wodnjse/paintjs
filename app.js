c9105582b1d6157c702e7bdcb846dd1cdcfb53ff
wodnjse <macimaro04@gmail.com>
2022-08-24 23:33:53 +0900

    2-4 Brush Size

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");  // canvas안에서의 픽셀을 다룰 수 있게 함
 const colors =  document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -9,6 +11,7 @@ ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
     painting = false;
@@ -31,9 +34,25 @@ function onMouseMove(event) {  // 캔버스 안에서의 마우스 좌표
 }
 
 
-function handelColorClick(event) {
+function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+}
+
+function handleRangeChange(event) {
+    const size = event.target.valueAsNumber;
+    ctx.lineWidth = size;
+}
+
+/* 채우기(filling)모드인지 아닌지 확인 */
+function handleModeClick() {
+    if (filling === true) {
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Paint";
+    }
 }
 
 /*
@@ -50,5 +69,13 @@ if (canvas) {
 }
 
 Array.from(colors).forEach(color => 
-    color.addEventListener("click", handelColorClick)
+    color.addEventListener("click", handleColorClick)
 );
+
+if (range) {
+    range.addEventListener("input", handleRangeChange)
+}
+
+if (mode) {
+    mode.addEventListener("click", handleModeClick);
+}
