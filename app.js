c30d5956c8b4488a9e72b526839765cbf36b4060
wodnjse <macimaro04@gmail.com>
2022-10-04 00:57:38 +0900

    2-5 Filling Mode

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const canvas = document.getElementById("jsCanvas");
-const ctx = canvas.getContext("2d");  // canvas안에서의 픽셀을 다룰 수 있게 함
-const colors =  document.getElementsByClassName("jsColor");
+const ctx = canvas.getContext("2d"); // canvas안에서의 픽셀을 다룰 수 있게 함
+const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -21,7 +25,8 @@ function startPainting() {
     painting = true;
 }
 
-function onMouseMove(event) {  // 캔버스 안에서의 마우스 좌표
+function onMouseMove(event) {
+    // 캔버스 안에서의 마우스 좌표
     const x = event.offsetX;
     const y = event.offsetY;
     if (!painting) {
@@ -33,10 +38,10 @@ function onMouseMove(event) {  // 캔버스 안에서의 마우스 좌표
     }
 }
 
-
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
@@ -61,19 +66,26 @@ function onMouseUp(event) {  // 마우스를 땠을 때, 칠하기를 멈춤
 }
 */
 
+function handleCanvasClick() {
+    if (filling) {
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    }
+}
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
 }
 
-Array.from(colors).forEach(color => 
+Array.from(colors).forEach((color) =>
     color.addEventListener("click", handleColorClick)
 );
 
 if (range) {
-    range.addEventListener("input", handleRangeChange)
+    range.addEventListener("input", handleRangeChange);
 }
 
 if (mode) {
