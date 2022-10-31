8b6f1d5ea401f7a3a27fe99ec4a6b26577a81033
wodnjse <macimaro04@gmail.com>
2022-10-31 23:23:12 +0900

    2-6 Saving the Image

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d"); // canvas안에서의 픽셀을 다룰 수 
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
@@ -10,6 +11,8 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = "white"  // "캔버스"의 기본 배경색
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);  // 배경이 transparent로 저장되는것 막아줌()
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -72,12 +75,34 @@ function handleCanvasClick() {
     }
 }
 
+/**
+ * 우클릭 방지
+ * @param {*} event 
+ * 
+ */
+function handleCM(event) {
+    event.preventDefault()
+}
+
+/**
+ * 이미지 다운로드
+ * @see https://developer.mozilla.org/ko/docs/Web/API/Event/preventDefault
+ */
+function handleSaveClick() {
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");  // html a태그 생성
+    link.href = image;
+    link.download = "PaintJS[🎨]";
+    link.click();
+}
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM)
 }
 
 Array.from(colors).forEach((color) =>
@@ -90,4 +115,8 @@ if (range) {
 
 if (mode) {
     mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick);
 }
